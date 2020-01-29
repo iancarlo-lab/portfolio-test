fe0929a0761fe0a7a87bcf6dc526eef114ae7eb8
Adrian <murilloadrianc@gmail.com>
2020-01-29 11:28:58 -0300

    Updates

---
+++ precache-manifest.1fb4fbef9be06ab6b3e6e0b41c50b92a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdfc6dfa8bfb1132907e8116fa6637e",
+    "url": "https//iancarlo-lab.github.io/portfolio-test/index.html"
+  },
+  {
+    "revision": "97722b636fe2674d2cf2",
+    "url": "https//iancarlo-lab.github.io/portfolio-test/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "ea1097bb35395b488800",
+    "url": "https//iancarlo-lab.github.io/portfolio-test/static/css/main.8c17eeb6.chunk.css"
+  },
+  {
+    "revision": "97722b636fe2674d2cf2",
+    "url": "https//iancarlo-lab.github.io/portfolio-test/static/js/2.e0c367d9.chunk.js"
+  },
+  {
+    "revision": "518fe96506d8e3a23395664a23f421e8",
+    "url": "https//iancarlo-lab.github.io/portfolio-test/static/js/2.e0c367d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea1097bb35395b488800",
+    "url": "https//iancarlo-lab.github.io/portfolio-test/static/js/main.1740cd9b.chunk.js"
+  },
+  {
+    "revision": "bde10b3e4331170c2ff6",
+    "url": "https//iancarlo-lab.github.io/portfolio-test/static/js/runtime-main.80d027cf.js"
+  }
+]);
